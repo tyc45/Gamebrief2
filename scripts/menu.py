485f0bad299760af37949bbf3a502c336e02e19c
Thanh Lan <manondoublier@yahoo.fr>
2021-12-08 13:46:30 +0100

    delete branch

---
+++ scripts/menu.py
@@ -0,0 +1,10 @@
+@dataclass
+class Menu():
+    player_name: str
+
+    def welcome_player():
+      """ input for git"""
+      player_name = intput("Hello! What is your name, adventurer?")
+      return player_name
+
+    
